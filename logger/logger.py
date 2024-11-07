75248a4a3a1f5224e1824555a49f11562ef2b180
Matthieu Vannin <matthieu.vannin@alchimiedatasolutions.com>
2024-11-07 11:43:55 +0100

    small changes logger

--- logger/logger.py
+++ logger/logger.py
@@ -1,7 +1,14 @@
 from CommonLib import *
 
-logger = ads.Logger(None, ads.Logger.DEBUG, "Logger")
+logger = ads.Logger(None, ads.Logger.DEBUG, "Logger",
+                    "LOGS", "LOGS_details")
 logger.info("Début de la démonstration.")
+
+# Le logger n'a pas de connexion pour ses logs
+logger.set_connection(source_pg)
+
+# Si les tables de logs n'existent pas, il faut appeler
+logger.create_logs_tables()
 
 logger.error("Message d'erreur")
 
@@ -15,4 +22,8 @@ logger.disable() # Cette commande désactive l'affichage des logs et l'insertion
 logger.info("Toi tu ne t'afficheras pas")
 
 logger.enable() # Par défaut, se réactive avec le niveau INFO
-logger.info("Fin de la démonstration !")
+
+# Avec chaque opération sur une base ads, une ligne sera insérée dans la table LOGS_details
+# Pour effectuer une insertion dans la table de logs principale, il faut appeler
+logger.log_close("DEMO", "Message type")
+# Attention log_close désactive les logs, logger.enable() si besoin
