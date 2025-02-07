18137e15abfb3984f1007624b1473a51015b3001
Matthieu Vannin <matthieu.vannin@alchimiedatasolutions.com>
2025-02-07 16:29:22 +0100

    update for 1.0.35

--- mail/mail.py
+++ mail/mail.py
@@ -1,21 +1,26 @@
-import adsToolBox as ads
+from CommonLib import *
 
+mailReader = ads.mail({
+    "url": env.MAIL_URL,
+    "clientId": env.MAIL_CLIENT_ID,
+    "clientSecret": env.MAIL_CLIENT_SECRET,
+    "email": env.MAIL,
+    "scope": "https://outlook.office365.com/.default",
+    "server": "outlook.office365.com",
+}, logger, 'oauth2')
 
-logger = ads.Logger(logLevel = ads.Logger.DEBUG, logger_name="mon_logger")
-MonMail = mail(dictionnary={"clientId":"",
-                  "clientSecret":"",
-                  "url":"",
-                  "email":"",
-                  "scope":"",
-                  "server":""},
-                logger=logger)
+email_ids = mailReader.get_email(mailbox="INBOX", readonly=True, emailFrom=env.MAIL_FROM, flag="UNSEEN")
 
-MonMail.connect_with_token()
-data = MonMail.get_email(mailbox="INBOX",readonly=True,emailFrom="chavegrandn@chavegrand.com")
-if data:
-    for element in data:
-        content=MonMail.read_email(element,get_attachment=True)
-        print(content)
-        break
+print(f"{len(email_ids)} emails non lus de {env.MAIL_FROM}")
 
-MonMail.logout()
+if email_ids:
+    email_id = email_ids[0]
+    email_content = mailReader.read_email(email_id, get_attachment=True)
+    print(f"📩 Contenu du mail:\n{email_content['message']}")
+    if "attachement" in email_content:
+        for attachement in email_content["attachement"]:
+            print(f"Pièce jointe: {attachement['filename']}")
+    mailReader.mark_as_seen(email_id)
+    mailReader.mark_as_un_seen(email_id)
+    mailReader.delete(email_id)
+mailReader.logout()
